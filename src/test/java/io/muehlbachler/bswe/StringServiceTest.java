cd9a3940d4da9165e678bb8c46d948b5257ddaea
Jan Jungwirth <125290988+janjungwirth@users.noreply.github.com>
2024-10-18 20:34:12 +0200

    added testing for StringService

--- src/test/java/io/muehlbachler/bswe/StringServiceTest.java
+++ src/test/java/io/muehlbachler/bswe/StringServiceTest.java
@@ -1,6 +1,8 @@
 package io.muehlbachler.bswe;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringServiceTest {
@@ -11,5 +13,18 @@ public class StringServiceTest {
         assertEquals("ABC", stringService.toUpperCase("abc"), "abc toUpperCase should be ABC");
     }
 
-    // FIXME: implement more tests
+    @Test
+    public void testEmptyString(){
+        assertEquals("", stringService.toUpperCase(""));
+    }
+
+    @Test
+    public void testNullString(){
+        try{
+            stringService.toUpperCase(null);
+            fail();
+        }catch (NullPointerException e){
+            assertEquals("Cannot invoke \"String.toUpperCase()\" because \"string\" is null", e.getMessage());
+        }
+    }
 }
